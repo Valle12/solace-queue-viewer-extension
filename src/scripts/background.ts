6ce7527290bbf250399881e42fe4d041d73b9839
Valle12 <valentinkaczmarek@yahoo.de>
2024-02-02 01:47:55 +0100

    Insertion of buttons on correct page Does not work with build yet

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,5 +1,33 @@
 export class Background {
-  constructor() {}
+  tabURL!: string;
+  regex: RegExp;
+
+  constructor() {
+    this.regex = new RegExp(
+      "https://.*.messaging.solace.cloud:943/.*/endpoints/queues/.*/messages?",
+      "gm"
+    );
+
+    this.addListeners();
+  }
+
+  addListeners() {
+    chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, _tab) => {
+      if (changeInfo.status !== "complete") return;
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+      if (tab.url == undefined) return;
+      this.tabURL = tab.url;
+    });
+
+    chrome.runtime.onMessage.addListener(
+      (_message: string, _sender, sendResponse) => {
+        sendResponse(this.regex.test(this.tabURL));
+      }
+    );
+  }
 }
 
 new Background();
--- src/scripts/solace.ts
+++ src/scripts/solace.ts
@@ -1,3 +1,6 @@
+import "/node_modules/material-design-lite/material.min.css";
+import "/node_modules/material-design-lite/material.min.js";
+
 export type SolaceConfig = {
   host: string;
   password: string;
@@ -7,28 +10,68 @@ export type SolaceConfig = {
 
 export class Solace {
   solaceConfig!: SolaceConfig;
+  configLoaded = false;
+  buttonsInserted = false;
 
   constructor() {
-    document.body.addEventListener("click", () => {
-      this.loadConfig();
-    });
+    document.body.addEventListener("click", () => this.loadConfig());
   }
 
   async loadConfig() {
-    let extensionPrefix = "solaceQueueViewerExtension.";
-    let clusterDiv =
-      document.querySelector<HTMLDivElement>("div.data.title div");
-    if (clusterDiv == null) return;
-    let clusterPrefix = clusterDiv.innerText;
-    let prefix = extensionPrefix + clusterPrefix;
-    let values = await chrome.storage.local.get();
-    this.solaceConfig = {
-      host: values[`${prefix}.host`],
-      password: values[`${prefix}.password`],
-      vpn: values[`${prefix}.vpn`],
-      username: values[`${prefix}.username`],
-    };
-    console.log(this.solaceConfig);
+    if (!this.configLoaded) {
+      let extensionPrefix = "solaceQueueViewerExtension.";
+      let clusterDiv =
+        document.querySelector<HTMLDivElement>("div.data.title div");
+      if (clusterDiv == null) return;
+      let clusterPrefix = clusterDiv.innerText;
+      let prefix = extensionPrefix + clusterPrefix;
+      let values = await chrome.storage.local.get();
+      this.solaceConfig = {
+        host: values[`${prefix}.host`],
+        password: values[`${prefix}.password`],
+        vpn: values[`${prefix}.vpn`],
+        username: values[`${prefix}.username`],
+      };
+      this.configLoaded = true;
+    }
+
+    this.insertButtons();
+  }
+
+  async insertButtons() {
+    if (!this.buttonsInserted) {
+      if (!(await chrome.runtime.sendMessage("urlCheck"))) return;
+      let font = new FontFace(
+        "MaterialIcons",
+        "url(https://fonts.googleapis.com/icon?family=Material+Icons)"
+      );
+      document.fonts.add(font);
+      let startReceiving = document.createElement("button");
+      this.insertButton(startReceiving, "play_arrow");
+      let stopReceiving = document.createElement("button");
+      this.insertButton(stopReceiving, "stop");
+      let actionPanel = document.querySelector<HTMLUListElement>(
+        "ul.au-target.table-action-panel.nav.flex-nowrap"
+      );
+      if (actionPanel == null) return;
+      actionPanel.appendChild(startReceiving);
+      actionPanel.appendChild(stopReceiving);
+      this.buttonsInserted = true;
+    }
+  }
+
+  insertButton(button: HTMLButtonElement, icon: string) {
+    button.classList.add("mdl-button", "mdl-js-button", "mdl-button--icon");
+    button.style.margin = "0 5px 1px 5px";
+    button.style.minWidth = "0";
+    button.style.minHeight = "0";
+    button.style.width = "30px";
+    button.style.height = "30px";
+    button.style.color = "#00C895";
+    let buttonIcon = document.createElement("i");
+    buttonIcon.classList.add("material-icons");
+    buttonIcon.innerText = icon;
+    button.appendChild(buttonIcon);
   }
 }
 
